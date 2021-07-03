f6824e7107bf4903f895897e41bead8c6cc8cac1
Епифанов Сергей Владимирович <sergeysven1@gmail.com>
2021-07-03 11:25:10 +0300

    added eslint rules: import/order and paddings

--- js/8kyu/catYearsDogYears/humanYearsCatYearsDogYears.js
+++ js/8kyu/catYearsDogYears/humanYearsCatYearsDogYears.js
@@ -6,6 +6,7 @@ const restDogYears = 5;
 export const humanYearsCatYearsDogYears = (humanYears) => {
   const animalYears = firstAnimalYear + (humanYears > 1 ? 1 : 0) * secondAnimalYear;
   const restAnimalYears = humanYears > 2 ? humanYears - 2 : 0;
+
   return [
     humanYears,
     animalYears + restAnimalYears * restCatYears,
